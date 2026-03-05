0e85680b80aced27b27bc8ba5687de1b6eeb1bca
cristake007 <popadorincristian@gmail.com>
2026-03-05 20:57:21 +0200

    Enhance guided tour UX and harden suspend_data persistence

---
+++ scorm_v4_major_update/src/blocks/TimelineEventsBlock.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="scorm-card timelineBlock" :class="`timelineBlock--${orientation || 'vertical'}`">
+    <div v-if="title" class="scorm-h2 timelineBlock__title">{{ title }}</div>
+
+    <div class="timelineBlock__list">
+      <article v-for="(item, idx) in items" :key="item.id || idx" class="timelineBlock__item">
+        <div class="timelineBlock__dot" aria-hidden="true"></div>
+        <div class="timelineBlock__content">
+          <div class="timelineBlock__meta" v-if="item.date">{{ item.date }}</div>
+          <div class="timelineBlock__heading">{{ item.title }}</div>
+          <p class="timelineBlock__desc">{{ item.description }}</p>
+        </div>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  id?: string;
+  type: "timeline.events";
+  title?: string;
+  orientation?: "vertical" | "horizontal";
+  items: Array<{ id?: string; title: string; date?: string; description: string }>;
+}>();
+</script>
