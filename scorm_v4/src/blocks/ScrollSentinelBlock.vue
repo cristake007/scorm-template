51154c7e2ea328196f5a0099016ba898229c94fe
cristake007 <popadorincristian@gmail.com>
2026-03-05 15:08:51 +0200

    Delete scorm_v4 directory

--- scorm_v4/src/blocks/ScrollSentinelBlock.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="scorm-card" style="text-align:center">
-    <div class="scorm-muted">{{ text || "You reached the end of the chapter." }}</div>
-  </div>
-</template>
-
-<script setup lang="ts">
-defineProps<{ text?: string }>();
-</script>
